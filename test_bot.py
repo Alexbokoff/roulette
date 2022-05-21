5a17845cc33414cf89a8f2fe9e6d3dd6857c14b6
Bokov Alex <bokov.cyber@gmail.com>
2022-05-21 14:05:16 +0300

    add files to gitignore

--- test_bot.py
+++ test_bot.py
@@ -25,21 +25,22 @@ def delete_duplicate_number(next_numbers):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == 'next':
-        bot.send_message(call.message.chat.id, 'Введите следующее число:')
-        bot.register_next_step_handler(call.message, main)
-    elif call.data == 'list':
+    if call.data == 'list':
         keyboard = types.InlineKeyboardMarkup()
-        btn_go = types.InlineKeyboardButton(text='Начать работу', callback_data='go')
+        btn_go = types.InlineKeyboardButton(text='Продолжить работу', callback_data='go')
         length_list = types.InlineKeyboardButton(text='Длина списка', callback_data='length')
         keyboard.add(btn_go, length_list)
-        bot.send_message(call.message.chat.id, f'Список: {list_number}', reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, f'Список: {list_number} \n\n'
+                                               f'Работа приостановлена, для продолжения нажмите необходимую кнопку ниже:',
+                         reply_markup=keyboard)
     elif call.data == 'length':
         keyboard = types.InlineKeyboardMarkup()
-        btn_go = types.InlineKeyboardButton(text='Начать работу', callback_data='go')
+        btn_go = types.InlineKeyboardButton(text='Продолжить работу', callback_data='go')
         print_list = types.InlineKeyboardButton(text='Список чисел', callback_data='list')
         keyboard.add(btn_go, print_list)
-        bot.send_message(call.message.chat.id, f'Длина списка: {len(list_number)}', reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, f'Длина списка: {len(list_number)}\n\n'
+                                               f'Работа приостановлена, для продолжения нажмите необходимую кнопку ниже:',
+                         reply_markup=keyboard)
     elif call.data == 'go':
         bot.send_message(call.message.chat.id, 'Я готов к работе. Введите число:')
         bot.register_next_step_handler(call.message, main)
@@ -54,41 +55,52 @@ def start(message):
     keyboard.add(btn_go, print_list, length_list)
     bot.send_message(message.chat.id, 'Привет, я бот, который ведет статистику\n'
                                       'выпадения чисел в рулетке.\n'
-                                      'В процессе работы выйти в главное меню: /start\n\n'
+                                      'Вы находитесь в главном меню.\n'
+                                      'Тут можно посмотреть список чисел, а также длинну списка\n\n'
                                       'Для начала работы, нажми необходимую кнопку ниже:\n', reply_markup=keyboard)
 
 
 def main(message):
-    global number
-    number = int(message.text)
-    global list_number
-    answer_text = 'После введеного числа выпадали:\n'
-    list_number.append(number)
-    next_numbers = next_number_list(number, list_number)
-    duplicate_list = delete_duplicate_number(next_numbers)
-    if next_numbers == []:
-        keyboard = types.InlineKeyboardMarkup()
-        next_num = types.InlineKeyboardButton(text='Следующее число', callback_data='next')
-        print_list = types.InlineKeyboardButton(text='Список чисел', callback_data='list')
-        length_list = types.InlineKeyboardButton(text='Длина списка', callback_data='length')
-        keyboard.add(next_num, print_list, length_list)
-        bot.send_message(message.chat.id, 'Этого числа еще нет в статистике', reply_markup=keyboard)
+    if message.text == 'Главное меню':
+        start(message)
+    elif int(message.text) < 37 and int(message.text) >= 0:
+        global number
+        number = int(message.text)
+        global list_number
+        answer_text = 'После введеного числа выпадали:\n'
+        list_number.append(number)
+
+        add_number_to_list = str(list_number)
+        file = open('list_number_test_bot.txt', 'w')
+        file.write(add_number_to_list)
+        file.close()
+
+        next_numbers = next_number_list(number, list_number)
+        duplicate_list = delete_duplicate_number(next_numbers)
+        if next_numbers == []:
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            menu_btn = types.KeyboardButton('Главное меню')
+            keyboard.add(menu_btn)
+            bot.send_message(message.chat.id, 'Этого числа еще нет в статистике', reply_markup=keyboard)
+            bot.register_next_step_handler(message, main)
+        else:
+            for i in range(len(duplicate_list)):
+                count_num = next_numbers.count(duplicate_list[i])
+                percent = (count_num / len(next_numbers)) * 100
+                percent = round(percent, 2)
+                answer_text += 'Число ' + str(duplicate_list[i]) + ' ' + '(' + str(count_num) + 'раз) - ' + str(
+                    percent) + '%\n'
+                if i == len(duplicate_list) - 1:
+                    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                    menu_btn = types.KeyboardButton('Главное меню')
+                    keyboard.add(menu_btn)
+                    bot.send_message(message.chat.id, answer_text, reply_markup=keyboard)
+                    bot.register_next_step_handler(message, main)
+                else:
+                    continue
     else:
-        for i in range(len(duplicate_list)):
-            count_num = next_numbers.count(duplicate_list[i])
-            percent = (count_num / len(next_numbers)) * 100
-            percent = round(percent, 2)
-            answer_text += 'Число ' + str(duplicate_list[i]) + ' ' + '(' + str(count_num) + 'раз) - ' + str(
-                percent) + '%\n'
-            if i == len(duplicate_list) - 1:
-                keyboard = types.InlineKeyboardMarkup()
-                next_num = types.InlineKeyboardButton(text='Следующее число', callback_data='next')
-                print_list = types.InlineKeyboardButton(text='Список чисел', callback_data='list')
-                length_list = types.InlineKeyboardButton(text='Длина списка', callback_data='length')
-                keyboard.add(next_num, print_list, length_list)
-                bot.send_message(message.chat.id, answer_text, reply_markup=keyboard)
-            else:
-                continue
+        bot.send_message(message.chat.id, 'Неверное числовое значение, повторите ввод')
+        bot.register_next_step_handler(message, main)
 
 
 bot.infinity_polling()
